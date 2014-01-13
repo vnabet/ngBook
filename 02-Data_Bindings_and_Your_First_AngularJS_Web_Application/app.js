678f121b9b39ce90dab5effb77b67088f00658e4
vnabet <vnabet@gmail.com>
2014-01-13 13:47:31 +0100

    Chapter 2 is done

---
+++ 02-Data_Bindings_and_Your_First_AngularJS_Web_Application/app.js
@@ -0,0 +1,17 @@
+function MyController($scope) {
+
+  $scope.clock = {
+    now: new Date()
+  }
+
+  var updateClock = function() {
+    $scope.clock.now = new Date();
+  }
+
+  setInterval(function() { 
+    $scope.$apply(updateClock);
+    //updateClock();
+  }, 1000);
+
+  updateClock();
+}
